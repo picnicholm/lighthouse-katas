4eea6cf04c6f9bf2a480b74eea185242d89d7c07
Nick.Holm <nick416holm@protonmail.com>
2022-12-22 12:55:58 -0500

    refactor with replace method

--- urlEncode.js
+++ urlEncode.js
@@ -1,8 +1,9 @@
 //Convert text input to an array, trim whitespace off the edges, transform every instance of whitspace, and join back to string.
-const urlEncode = (text) =>
-  Array.from(text.trim())
-    .map((x) => (x !== " " ? x : "%20"))
-    .join("");
+// const urlEncode = (text) =>
+//   Array.from(text.trim())
+//     .map((x) => (x !== " " ? x : "%20"))
+//     .join("");
+const urlEncode = (text) => text.trim().replace(" ", "%20");
 
 console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode(" Lighthouse Labs "));
